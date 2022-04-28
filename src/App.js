3f819c64c283661f75fcbc3aa91a67963be424b9
KJH <stonehead334@gmail.com>
2022-04-28 14:39:51 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Button from "./Button";
 import style from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue(prev => prev + 1);
+  useEffect(() => {
+    console.log("Call the APi");
+  });
   return (
     <div>
-      <h1 className={style.title}>Welcome back!!!!!!!!!</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
@@ -13,3 +19,6 @@ function App() {
 export default App;
 
 //독립적으로 나눌 수 있어서 편하긴 하다
+/*
+useEffect는 코드가 딱 한번만 실행하도록 보호해준다
+*/
