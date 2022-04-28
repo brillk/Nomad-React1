5f25cbe152f857c17c50e618e2c4bdcee651ecc3
KJH <stonehead334@gmail.com>
2022-04-28 16:36:08 +0900

    To Do list part-2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       return;
     } //toDo 그러니까 state를 직접 바꾸면 안되고, func을 이용해 바꿔야 한다 무조건
     setToDos(currentArray => [toDo, ...currentArray]); // ...는 array에 있는 값을 합친다
-    setToDo("");
+    setToDo(""); //<- 이 값은 우리가 저장한 data를 갖고 있다
   };
   return (
     <div>
@@ -24,6 +24,23 @@ function App() {
         />
         <button>Add To do</button>
       </form>
+      <hr />
+      <ul>
+        {" "}
+        {/*map은 배열을 가지고 있을 때 각각의 element들을 바꿀 수 있게 해줌*/}
+        {toDos.map(
+          (
+            item,
+            index 
+            /*리액트는 기본적으로 list에 있는 모든 item을 인식하기 때문에
+            key를 넣어 고유하게 만들어줘야함
+            map의 첫 번째 argument는 값이고 두번째는 index 즉 숫자를 의미함
+             */
+          ) => (
+            <li key={index}>{item}</li>
+          )
+        )}
+      </ul>
     </div>
   );
 }
