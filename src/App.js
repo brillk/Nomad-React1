912995086b58d56155472af7bbf7926414afbb20
KJH <stonehead334@gmail.com>
2022-04-28 14:56:52 +0900

    Deps

--- src/App.js
+++ src/App.js
@@ -4,12 +4,26 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue(prev => prev + 1);
+  const onChange = event => setKeyword(event.target.value);
+  console.log("i ran all the time");
   useEffect(() => {
     console.log("Call the APi");
-  });
+  }, []);
+  useEffect(() => { // 특정한 부분을 실행하는 것을 세분화하고 조건문을 넣어 
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("search for", keyword);
+    }
+  }, [keyword]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here"
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
