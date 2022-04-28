c2032ed6b391a5568f3d74ec48f62cd49b21eeb0
KJH <stonehead334@gmail.com>
2022-04-28 17:33:13 +0900

    Coin Tracker...

--- src/App.js
+++ src/App.js
@@ -1,46 +1,60 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = event => setToDo(event.target.value);
-  const onSubmit = event => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    } //toDo 그러니까 state를 직접 바꾸면 안되고, func을 이용해 바꿔야 한다 무조건
-    setToDos(currentArray => [toDo, ...currentArray]); // ...는 array에 있는 값을 합친다
-    setToDo(""); //<- 이 값은 우리가 저장한 data를 갖고 있다
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [amount, setAmount] = useState(1);
+  const [need, setNeed] = useState(1);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
+      .then(response => response.json())
+      .then(json => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  const onChange = event => {
+    setAmount(event.target.value);
+    setNeed(1);
   };
+
+  const handleInput = event => {
+    setNeed(event.target.value);
+  };
+
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
+      <h1>The coins!{loading ? "" : `Here are..${coins.length} coins`}</h1>
+      {loading ? (
+        <strong>loading...</strong>
+      ) : (
+        <select onChange={onChange}>
+          <option>Select Coin!</option>
+          {coins.map((coin, index) => (
+            <option
+              key={index}
+              value={coin.quotes.USD.price}
+              id={coin.symbol}
+              symbol={coin.symbol}
+            >
+              {coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+      <h2>Please enter the amount</h2>
+      <div>
         <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do"
+          type="number"
+          value={need}
+          onChange={handleInput}
+          placeholder="dollor"
         />
-        <button>Add To do</button>
-      </form>
-      <hr />
-      <ul>
-        {" "}
-        {/*map은 배열을 가지고 있을 때 각각의 element들을 바꿀 수 있게 해줌*/}
-        {toDos.map(
-          (
-            item,
-            index 
-            /*리액트는 기본적으로 list에 있는 모든 item을 인식하기 때문에
-            key를 넣어 고유하게 만들어줘야함
-            map의 첫 번째 argument는 값이고 두번째는 index 즉 숫자를 의미함
-             */
-          ) => (
-            <li key={index}>{item}</li>
-          )
-        )}
-      </ul>
+        $
+      </div>
+      <h2>You can get {need / amount}</h2>
     </div>
   );
 }
