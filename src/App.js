b6cdecfd07c765b90aee30abfde3f400bdc82df3
KJH <stonehead334@gmail.com>
2022-04-29 09:41:37 +0900

    Movie App part-1

--- src/App.js
+++ src/App.js
@@ -2,59 +2,39 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [amount, setAmount] = useState(1);
-  const [need, setNeed] = useState(1);
-
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9.2&sort_by=year"
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  }; //정보를 가져왔으니 로딩을 끊어야 한다
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
-      .then(response => response.json())
-      .then(json => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-
-  const onChange = event => {
-    setAmount(event.target.value);
-    setNeed(1);
-  };
-
-  const handleInput = event => {
-    setNeed(event.target.value);
-  };
-
+    getMovies();
+  }, []); //리스트가 보이게 map을 쓰자 / array 변수에 각각 적용
   return (
     <div>
-      <h1>The coins!{loading ? "" : `Here are..${coins.length} coins`}</h1>
       {loading ? (
-        <strong>loading...</strong>
+        <h1>Loading...</h1>
       ) : (
-        <select onChange={onChange}>
-          <option>Select Coin!</option>
-          {coins.map((coin, index) => (
-            <option
-              key={index}
-              value={coin.quotes.USD.price}
-              id={coin.symbol}
-              symbol={coin.symbol}
-            >
-              {coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD
-            </option>
+        <div>
+          {movies.map(movie => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>Released Date: {movie.torrents[0].date_uploaded}</p>
+              <p>Runtime: {movie.runtime} minutes</p>
+              <ul>
+                {movie.genres.map(g => (
+                  <li key={g}>{g}</li> //각 영화의 장르를 유니크화시켜서
+                ))}
+              </ul>
+            </div>
           ))}
-        </select>
+        </div>
       )}
-      <h2>Please enter the amount</h2>
-      <div>
-        <input
-          type="number"
-          value={need}
-          onChange={handleInput}
-          placeholder="dollor"
-        />
-        $
-      </div>
-      <h2>You can get {need / amount}</h2>
     </div>
   );
 }
